0f53c365aebd2a622e70b79f2c29f7d6c4846fa0
Ted Cassirer <ted.cassirer@aphelion.se>
2016-04-27 16:15:16 +0200

    e

--- email_logs/log_parser.py
+++ email_logs/log_parser.py
@@ -62,10 +62,11 @@ def processFile(filePath):
             registerException(currentMatch)
 
 
-def send_mail(send_from, send_to, subject, text, files=None):
+def send_mail(send_to, subject, text, files=None):
+    my_email = "ted.cassirer@gmail.com"
     assert isinstance(send_to, list)
     msg = MIMEMultipart(
-        From=send_from,
+        From=my_email,
         To=', '.join(send_to),
         Date=formatdate(localtime=True),
         Subject=subject
@@ -86,53 +87,65 @@ def send_mail(send_from, send_to, subject, text, files=None):
     import base64
     # This is very safe and uncrackable. base64 has not been solved yet.
     pw = base64.b64decode(open("pw").read())
-    smtp.login(send_from, pw)
-    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.login(my_email, pw)
+    smtp.sendmail(my_email, send_to, msg.as_string())
     smtp.close()
 
+
+def find_logs(location, name_filter, file_list=[]):
+    directory_content = [path.join(location, f) for f in os.listdir(location)]
+    for f in directory_content:
+        if path.isfile(f) and name_filter in f:
+            file_list.append(f)
+        elif path.isdir(f):
+            file_list = find_logs(f, name_filter, file_list)
+    return file_list
 
 
 def main(argv):
     output_directory = "exception_logs"
     input_directory = "logs"
-    sender = "ted.cassirer@gmail.com"
+    name_filter = ""
     try:
-        opts, args = getopt.getopt(argv, "i:o:s:r:", ["input_directory=", "output_directory=", "sender=", "receiver="])
+        opts, args = getopt.getopt(argv, "i:o:f:r:", ["input_directory=", "output_directory=", "name_filter=", "receiver="])
     except getopt.GetoptError:
-        print 'log_parser.py -i <input directory> -o <output directory> -s <sender> -r <receivers>'
-        print "Example: python log_parser -i logs/application -o output_directory/ -s my.mail@gmail.com " \
+        print 'Input ERROR:\nlog_parser.py -i <input directory> -o <output directory> -f <file filter> -r <receivers>'
+        print "\nExample: python log_parser -i logs/application -o output_directory/ -f QFX " \
               "-r person@mail.com,other.guy@mail.com"
         sys.exit(2)
     for opt, arg in opts:
         if opt in ("-i", "--input"):
             input_directory = arg
+        elif opt in ("-f", "--name_filter"):
+            name_filter = arg
         elif opt in ("-o", "--output"):
             output_directory = arg
-        elif opt in ("-s", "--sender"):
-            sender = arg
         elif opt in ("-r", "--receivers"):
             receivers = arg.split(',')
         else:
             assert False, "unhandled option"
+            sys.exit(2)
 
-    logs = [path.join(input_directory, f) for f in os.listdir(input_directory)]
+    logs = find_logs(input_directory, name_filter)
+    print logs
     new_files = []
     for f in logs:
         processFile(f)
-        i = f.find(path.sep)
+        i = f.rfind(path.sep)
         if not path.exists(output_directory):
             os.makedirs(output_directory)
         new_file = path.join(output_directory, DATE_TODAY.isoformat()[:10] + "_exceptions_" + f[i + 1:])
+        print new_file
         new_files.append(new_file)
         with open(new_file, "w") as output:
             for exception in sorted(exceptions.items(), key=lambda e: e[1][0], reverse=True):
-                out = DATE_TODAY.isoformat()[:10] + \
-                      "\n{count} found:\n{head}\n{trace}\n{time}".format(
+                out = "log path: " + f + "\n" + DATE_TODAY.isoformat()[:10] + \
+                      "\n\n{count} found:\n{head}\n{trace}\n{time}".format(
                           count=exception[1][0], head=exception[1][2][0], trace=exception[0], time=str(exception[1][1])) + \
                       "\n\n" + 100 * "-" + "\n"
                 output.write(out)
 
-    send_mail(sender, receivers, "Exceptions'n stuff", "It worked!", new_files)
+    send_mail(receivers, "Exceptions'n stuff", "It worked!", new_files)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
